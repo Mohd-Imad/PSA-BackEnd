6f0fc18e28b1727d3916fe6610cb7094c8390319
Mohd-Imad <mohdimd123@gmail.com>
2022-11-18 18:03:11 +0530

    ok

---
+++ Inbuilt-Modules/OS-Module/os_module.js
@@ -0,0 +1,10 @@
+const os = require('os')         //ES5
+
+
+console.log(os.arch());           //x64
+console.log(os.platform());       //win32
+console.log(os.version());        //Windows 10 
+console.log(os.userInfo());  
+     
+
+
