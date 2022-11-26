c68bc74965938e0039ca8a1c8c133fd7c2e30223
Mohd-Imad <mohdimd123@gmail.com>
2022-11-26 18:35:47 +0530

    presetting env

--- 3rdParty-Modules/Express JS-ES6/app.js
+++ 3rdParty-Modules/Express JS-ES6/app.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app  = express()
+import express from 'express'
+// const express = require('express')
+const app = express()
 
 app.get('/',(req,resp)=>{
-    resp.send('<h1>Routing</h1>')
+    resp.send("<h1>Hi this is ES6</h1>")
 })
 
 
 app.listen(8020,()=>{
     console.log(`Server is running on http://localhost:8020`);
+    
 })
