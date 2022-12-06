c20055bc19e7b8faf6c8b0d6afe910a519865747
Mohd-Imad <mohdimd123@gmail.com>
2022-12-06 21:52:53 +0530

    ok

--- MongoDB/Class-2-CRUD/app.js
+++ MongoDB/Class-2-CRUD/app.js
@@ -1,11 +1,22 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import chalk from 'chalk'
 
 const app = express()
+
+dotenv.config({path : './Config/.env'})
+let port = process.env.PORT 
+let host = process.env.HOST_NAME
+
+//http logger
+app.use(morgan('common'))
 
 app.get('/',(req,resp)=>{
     resp.send("<h1>Learning CRUD operations using Node JS and Mongo DB</h1>")
 })
 
-app.listen(8030,()=>{
-    console.log(`Server is running on http://localhost:8030`);
+app.listen(port,host,()=>{
+    console.log(chalk.yellow(`Server is running on http://${host}:${port}`));
+    console.log(chalk.yellow(`Server is running on http://localhost:${port}`));
 })
