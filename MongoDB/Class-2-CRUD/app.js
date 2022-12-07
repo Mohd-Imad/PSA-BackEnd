c5b12a729dc251fdb2a14bb2397e5dd769920101
Mohd-Imad <mohdimd123@gmail.com>
2022-12-07 20:06:20 +0530

    ok

--- MongoDB/Class-2-CRUD/app.js
+++ MongoDB/Class-2-CRUD/app.js
@@ -1,20 +1,28 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
+import cors from 'cors'
 import chalk from 'chalk'
+import mongoose from 'mongoose'
 
 const app = express()
 
 dotenv.config({path : './Config/.env'})
-let port = process.env.PORT 
-let host = process.env.HOST_NAME
 
-//http logger
-app.use(morgan('common'))
+app.use(cors()) //to enable client access
+app.use(morgan('short')) //http logger
 
+//to read form data
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+//route request
 app.get('/',(req,resp)=>{
     resp.send("<h1>Learning CRUD operations using Node JS and Mongo DB</h1>")
 })
+
+const port = process.env.PORT 
+const host = process.env.HOST_NAME
 
 app.listen(port,host,()=>{
     console.log(chalk.yellow(`Server is running on http://${host}:${port}`));
---
+++ MongoDB/Class-2-CRUD/productRouter.js
@@ -0,0 +1,9 @@
+import express from 'express'
+const router = express.Router()
+
+
+router.get('/',(req,resp)=>{
+    
+})
+
+export default router
