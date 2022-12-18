4248d0e996c3fbd61f50b14615041ccd525b9034
Mohd-Imad <mohdimd123@gmail.com>
2022-12-18 22:53:23 +0530

    completed CRUD-operations

--- MongoDB/Class-2-CRUD/router/productRouter.js
+++ MongoDB/Class-2-CRUD/router/productRouter.js
@@ -16,14 +16,14 @@ router.post('/create', async (req, resp) => {
             price: req.body.price,
             qty: req.body.qty,
         }
-    
+
         //for avoiding existing product creation
         let product = await Product.findOne({ name: new_product.name })
         if (product) {
             return resp.status(401).json({ msg: "Product already exists...!" })
         }
 
-          product = Product(new_product)
+        product = Product(new_product)
         console.log(product);
 
         product = await product.save()    //save to DB
@@ -33,7 +33,29 @@ router.post('/create', async (req, resp) => {
         })
     }
     catch (err) {
-        if(err) throw err
+        if (err) throw err
+    }
+})
+
+/*
+    URL:localhost:8030/products/product/:id
+    Method: GET
+    Fields:name, price, qty
+*/
+router.get('/product/:id', async (req, resp) => {
+    try {
+        let product_Id = req.params.id
+        let product = Product.findById(product_Id)
+        if (!product) {
+            return resp.status(401).json({
+                result: "No Product...!"
+            })
+        }
+        product = await Product.findByIdAndDelete((product_Id))
+        resp.status(200).json(product)
+    }
+    catch (err) {
+        resp.status(500).json({ msg: err.message })
     }
 })
 
@@ -42,13 +64,13 @@ router.post('/create', async (req, resp) => {
     Method: GET
     Fields:name, price, qty
 */
-router.get('/all',async(req,resp)=>{
-    try{
-       let products = await Product.find()
-       resp.status(200).json(products)
+router.get('/all', async (req, resp) => {
+    try {
+        let products = await Product.find()
+        resp.status(200).json(products)
     }
-    catch(err){
-        resp.status(500).json({msg:err.message})
+    catch (err) {
+        resp.status(500).json({ msg: err.message})
     }
 })
 
@@ -57,24 +79,25 @@ router.get('/all',async(req,resp)=>{
     Method: PUT
     Fields:name, price, qty
 */
-router.put('/update/:id', async (req,resp)=>{
+router.put('/update/:id', async (req, resp) => {
     let product_Id = req.params.id
     console.log(product_Id);
-    try{
+    try {
         let updatedProduct = {
-            name : req.body.name,
-            price : req.body.price,
-            qty : req.body.qty
+            name: req.body.name,
+            price: req.body.price,
+            qty: req.body.qty
         }
         let product = Product.findById(product_Id)
-        if(!product){
-            return resp.status(401).json({msg : "No Product Found...!"})
+        if (!product) {
+            return resp.status(401).json({ msg: "No Product Found...!" })
         }
-       product = await Product.findByIdAndUpdate(product_Id,{$set:updatedProduct})
-       resp.status(200).json({result : "Product updated succesfully...!",product : product})
+        product = await Product.findByIdAndUpdate(product_Id, { $set: updatedProduct })
+        resp.status(200).json({ result: "Product updated succesfully...!" }), { product: product }
     }
-    catch(err){
-        throw err
+    catch (err) {
+        resp.status(401).json({ msg: "No Product Found...!" })
+
     }
 })
 
@@ -83,21 +106,25 @@ router.put('/update/:id', async (req,resp)=>{
     Method: DELETE
     Fields:name, price, qty
 */
-router.delete('/delete/:id', async (req,resp)=>{
-    try{
+router.delete('/delete/:id', async (req, resp) => {
+    try {
         let product_Id = req.params.id
         let product = Product.findById(product_Id)
-        if(!product){
+        if (!product) {
             return resp.status(401).json({
-                result : "No Product...!"
+                result: "No Product...!"
             })
         }
         product = await Product.findByIdAndDelete((product_Id))
         resp.status(200).json({
-            result : "Product Deleted"
+            result: "Product Deleted"
         })
     }
-    catch(err){}
+    catch (err) {
+        resp.status(401).json({
+            result: "No Product found...!"
+        })
+    }
 })
 
 export default router
